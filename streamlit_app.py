2b84aff51e889b8305dd57f0db847d95e5dbb215
Andy Marchese <an.marchese@gmail.com>
2023-02-16 17:05:15 -0500

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,12 @@ df=df.sort_values(['Ratings','Title'],ascending=[False,True])
 df['Rating'] = df['Ratings'].astype(int)
 df.drop('Ratings',axis=1,inplace=True)
 df.reset_index(drop=True,inplace=True)
+add_selectbox = st.sidebar.selectbox(
+    "Movie Type?",
+    ("Documentary", "Action")
+)
 st.title('Peacock Documentary Ratings')
-st.table(df)
+if add_selectbox is None:
+    st.table(df)
+else:
+    st.table(df[df['Type']==add_selectbox])
