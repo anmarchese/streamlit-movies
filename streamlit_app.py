541abee25961e3c28aa71bc4bba9a7e1e8625d3e
Andy Marchese <an.marchese@gmail.com>
2023-02-16 15:52:36 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('peacock_doc_ratings.csv')
+df = pd.read_csv('movie_ratings.csv')
 df=df.sort_values(['Ratings','Title'],ascending=[False,True])
 df['Rating'] = df['Ratings'].astype(int)
 df.drop('Ratings',axis=1,inplace=True)
