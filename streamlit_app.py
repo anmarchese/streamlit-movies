61db358a73e8a513a6815355e063c45372ad1248
Andy Marchese <an.marchese@gmail.com>
2023-02-16 17:09:45 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,10 +7,6 @@ df = df.sort_values(['Ratings','Title'],ascending=[False,True])
 df['Rating'] = df['Ratings'].astype(int)
 df.drop('Ratings',axis=1,inplace=True)
 df.reset_index(drop=True,inplace=True)
-add_selectbox = st.sidebar.selectbox(
-    "Movie Type?",
-    ("Documentary", "Action")
-)
 types = df.Type.unique().tolist()
 add_selectbox = st.multiselect(
     'Movie Type?',
