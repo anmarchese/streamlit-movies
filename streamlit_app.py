f670bf569710e1d5c2fb9b9ae72a5150cf04b6d5
Andy Marchese <an.marchese@gmail.com>
2023-02-16 17:09:18 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv('movie_ratings.csv')
-df=df.sort_values(['Ratings','Title'],ascending=[False,True])
+df = df.sort_values(['Ratings','Title'],ascending=[False,True])
 df['Rating'] = df['Ratings'].astype(int)
 df.drop('Ratings',axis=1,inplace=True)
 df.reset_index(drop=True,inplace=True)
@@ -11,8 +11,12 @@ add_selectbox = st.sidebar.selectbox(
     "Movie Type?",
     ("Documentary", "Action")
 )
-st.title('Peacock Documentary Ratings')
+types = df.Type.unique().tolist()
+add_selectbox = st.multiselect(
+    'Movie Type?',
+    types,types)
+st.title('Peacock Ratings')
 if add_selectbox is None:
     st.table(df)
 else:
-    st.table(df[df['Type']==add_selectbox])
+    st.table(df[df['Type'].isin(add_selectbox)])
