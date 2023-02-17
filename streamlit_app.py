b350379954551abbcc03c9b3eaacd51edcd57693
Andy Marchese <an.marchese@gmail.com>
2023-02-17 13:25:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,11 @@ df = df.sort_values(['IMDB_Rating','Audience_Rating','Title'],ascending=[False,F
 df['Audience_Rating'] = df['Audience_Rating'].astype(pd.Int64Dtype())
 df['Tomato_Rating'] = df['Tomato_Rating'].astype(pd.Int64Dtype())
 df['IMDB Vote Count'] = df['IMDB Vote Count'].astype(pd.Int64Dtype())
+#convert all the genre columns to 0 or 1
+for i in df.columns:
+    if 'Genre_' in i:
+        df[i] = df[i].fillna(0)
+        df[i] = df[i].astype(int)
 df['IMDB Vote Count'] = df['IMDB Vote Count'].fillna(0)
 df.drop('IMDB Matched Movie',axis=1,inplace=True)
 df.reset_index(drop=True,inplace=True)
