30205d0c41915139035b46ec4f3547494df80616
Andy Marchese <an.marchese@gmail.com>
2023-02-16 19:27:21 -0500

    slider

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,14 +6,19 @@ df = pd.read_csv('movie_ratings.csv')
 df = df.sort_values(['IMDB_Rating','Audience_Rating','Title'],ascending=[False,False,True])
 df['Audience_Rating'] = df['Audience_Rating'].astype(pd.Int64Dtype())
 df['Tomato_Rating'] = df['Tomato_Rating'].astype(pd.Int64Dtype())
+df['IMDB Vote Count'] = np.random.randint(0,100000,df.shape[0])
+df['IMDB Vote Count'] = df['IMDB Vote Count'].astype(pd.Int64Dtype())
 df.drop('IMDB Matched Movie',axis=1,inplace=True)
 df.reset_index(drop=True,inplace=True)
 types = df.Type.unique().tolist()
 add_selectbox = st.multiselect(
     'Movie Type?',
     types,types)
+min_votes = st.slider(
+    "Minimum IMDB Votes",
+    value=50, min_value=0, max_value=10000, step=10)
 st.title('Peacock Ratings')
 if add_selectbox is None:
     st.table(df)
 else:
-    st.table(df[df['Type'].isin(add_selectbox)])
+    st.table(df[df['Type'].isin(add_selectbox) & (df['IMDB Vote Count'] >= min_votes)])
